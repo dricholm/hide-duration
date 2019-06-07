34a965967be8a9098fd3cb813a85928a21709744
David Richolm <david.richolm@gmail.com>
2019-06-07 14:11:59 +0200

    Remove commented code

--- src/services/browser.ts
+++ src/services/browser.ts
@@ -6,13 +6,6 @@ export const setChannels: (
   channels: Array<Channel>
 ) => Promise<void> = async channels => browser.storage.sync.set({ channels });
 
-// export const getChannels: () => Promise<Array<Channel>> = () =>
-//   new Promise(resolve =>
-//     browser.storage.sync.get({ channels: [] }, items =>
-//       resolve(items.channels as Array<Channel>)
-//     )
-//   );
-
 export const getChannels: () => Promise<Array<Channel>> = async () => {
   const { channels } = await browser.storage.sync.get({ channels: [] });
   return channels;
